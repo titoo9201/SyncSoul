f0ce5027d28a14248f9f47819183dd6d2379d887
titoo9201 <titoos675@gmail.com>
2025-12-24 17:30:35 +0530

    backend setup

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import morgan from 'morgan';
+
+const app=express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
+export default app;
